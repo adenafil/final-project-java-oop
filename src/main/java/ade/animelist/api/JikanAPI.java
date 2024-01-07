13d337131848e2857ecc9a3b9fe44da1694042e9
adenafil <nafilie9@gmail.com>
2024-01-07 15:48:18 +0700

    update docs API

--- src/main/java/ade/animelist/api/JikanAPI.java
+++ src/main/java/ade/animelist/api/JikanAPI.java
@@ -1,6 +1,5 @@
 package ade.animelist.api;
 
-import ade.animelist.components.utilcomponent.ImageRenderer;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.Expiry;
@@ -11,26 +10,29 @@ import net.sandrohc.jikan.model.anime.Anime;
 import net.sandrohc.jikan.model.enums.AgeRating;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import javax.swing.*;
-import java.io.IOException;
 import java.time.OffsetDateTime;
 import java.util.*;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
+/**
+ * Class ini berisi method method untuk mengquery atau mengfetch API dari JIkan API (unofficaial MAL).
+ * Kami menggunakan "Java wrapper for the Jikan API V4" untuk mempermudah dalam membungkus sebuah hasil query yang
+ * berberntuk JSON menjadi Object di java dengan class Anime.
+ * Dependency -> https://github.com/SandroHc/reactive-jikan
+ */
 public class JikanAPI {
-    // an api object from library -> https://github.com/SandroHc/reactive-jikan
+    // Membuat Object class Jikan
     private static Jikan jikan;
-    // activated jikanCache for a better perform
+    // Membuat Object untuk mengaktifkan caching
     private static JikanCache jikanCache;
 
-    // i activated all of the feature on the below statement
-    // everything i got from docs in https://github.com/SandroHc/reactive-jikan
-    // thanks for the developer, so i dont have fetch api manually with java OkHTTP
+    /**
+     * Mengkonfigurasi caching fitur dengan Caffeine.
+     * Docs : https://github.com/SandroHc/reactive-jikan
+     * Docs : https://github.com/ben-manes/caffeine
+     */
     static {
         jikanCache = new JikanCache() {
             protected final Cache<String, JikanValueHolder> cache = Caffeine.newBuilder().maximumSize(10_000).expireAfter(new JikanExpiry()).build();
@@ -72,155 +74,48 @@ public class JikanAPI {
     }
 
     /**
-     * This method is used to look for anime by title
-     * @param title => i.g : 'naruto', 'dr. stone', 'demon slayer' and etc
-     * @return list of all anime that got from api
+     * Method ini digunakan untuk mencari anime yang dibungkus menjadi ArrayList
+     * @param title -> Judul anime
+     * @return -> list atau kumpulan anime
      * @throws JikanQueryException
      */
     public static List<Anime> searchAnimeByTitle(String title) throws JikanQueryException {
-//        jikan = new Jikan();
-
-//        List<Anime> searchResult = jikan.query().anime().search().query(title).execute().collectList().block();
-
+        // Dengan objek jikan lakukakan query anime, cari anime dengan rating 13 diberi judul sesuai parameter kemudian execute
+        // dan jadikan lah menjadi collectionList
         return jikan.query().anime().search().rating(AgeRating.PG13).query(title).execute().collectList().block();
     }
 
-    /**
-     * method for creating data model
-     * @param animeList (an list of result that is got by api)
-     * @return object[][] to make model on table
-     * @throws IOException
-     */
 
     /**
-     * this method is depreceted, im not gonna deleted for now untill
-     * my project beeen finised by myself, so yeah the probelm of this method is
-     * it took so long to create ImageIcon, that is why the solution that i got is to
-     * parallel all that imageIcon been created.
-     * @param animeList
-     * @return a data model objek
-     * @throws IOException
-     */
-    @Deprecated
-    public static Object[][] animeToDataTable(List<Anime> animeList) throws IOException {
-        int totalAnime = animeList.size();
-        Object[][] result = new Object[totalAnime][6];
-        int i = 0;
-        for (Anime anime : animeList) {
-            result[i][0] = anime.malId;
-            result[i][1] = ImageRenderer.createImageIconByURL(anime.images.jpg.largeImageUrl);
-            result[i][2] = anime.title;
-            result[i][3] = anime.score;
-            result[i][4] = anime.rating;
-            result[i][5] = anime.episodes;
-            i++;
-        }
-
-        return result;
-    }
-
-    /**
-     * this is a new solution that i got when stuck from bad perform .
-     * honestly i've ever thought that will use lazy way in java
-     * but i got confused so i looked for in google, ai or anywhere
-     * about how to java asnyc, parallel and concurrent work
-     * @param animeList
-     * @return an list of data[] model
-     */
-    public static List<Object[]> animeToDataTableNew(List<Anime> animeList) {
-        List<CompletableFuture<Object[]>> futures = new ArrayList<>();
-
-        for (Anime anime : animeList) {
-            CompletableFuture<Object[]> future = CompletableFuture.supplyAsync(() -> {
-                ImageIcon imageIcon = ImageRenderer.createImageIconByURL(anime.images.jpg.largeImageUrl);
-//                    System.out.println(anime.title);
-                return new Object[]{
-                        anime.malId,
-                        imageIcon,
-                        anime.title,
-                        anime.score,
-                        anime.rating,
-                        anime.episodes
-                };
-            });
-
-            futures.add(future);
-        }
-
-        // Combine all CompletableFuture into one CompletableFuture representing all of them
-        CompletableFuture<Void> allOf = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
-
-        // Wait for all CompletableFuture to complete
-        try {
-            allOf.get(); // This will block until all tasks are completed
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
-
-        // Retrieve the results from CompletableFuture
-        List<Object[]> resultList = futures.stream()
-                .map(future -> {
-                    try {
-                        return future.get(); // Get the result from each CompletableFuture
-                    } catch (InterruptedException | ExecutionException e) {
-                        e.printStackTrace();
-                        return null;
-                    }
-                })
-                .filter(result -> result != null)
-                .toList();
-
-        return resultList;
-    }
-
-
-//    public static int lengthAnimeResultOfAPI(List<Anime> animeList) {
-//        int total = 0;
-//        for (Anime anime : animeList) {
-//            total++;
-//        }
-//        return total;
-//    }
-
-//    private static ImageIcon createImageIconByURL(String imageURL) throws IOException {
-//        // Implementasi createImageIconByURL
-//        // Pastikan bahwa ini sudah dioptimalkan sebaik mungkin
-//        Image image = ImageIO.read(new URL(imageURL));
-//        if (image != null) return new ImageIcon(image);
-//        throw new IOException("Failed to read image from URL: " + imageURL);
-//    }
-
-    /**
-     * this method is used for getting anime by the id of MAL(myAnimeList website)
-     * i dont use a subcribe method for this
-     * @param id => (an id from official MAL)
-     * @return () => an object Anime
+     * Method untuk mendapatkan anime dengan id tertentu
+     * @param id -> id anime pada website MAL
+     * @return -> Object Anime
      * @throws JikanQueryException
      */
     public static Anime getAnimeById(int id) throws JikanQueryException {
+        // Tolong carikan saya anime dengan id ini kemudian execute
         return jikan.query().anime().get(id).execute().block();
     }
 
-    public static List<Anime> getTitleAndImageAnimeBySearch(String title) throws JikanQueryException {
-        List<Anime> results = jikan.query().anime().search().rating(AgeRating.PG13)
-                .query(title)
-                .limit(1)
-                .execute()
-                .collectList()
-                .block();
-
-//        jikan.query().anime().search().rating(AgeRating.PG13).query()
-
-        return results;
-    }
-
+    /**
+     * Method ini untuk mendapatkan atau mencari anime dengan judul tertentu
+     * @param title -> Judul anime
+     * @return -> -> Kumpulan dari anime (MAX 25 yang didapat)
+     * @throws JikanQueryException
+     */
     public static Mono<List<Anime>> getTitleAndImageAnimeBySearchAsync(String title) throws JikanQueryException {
+        // Mencari anime dengan mengfilter yang harus ditemani oleh orang tua
         return jikan.query().anime().search().rating(AgeRating.PG13)
                 .query(title)
                 .execute()
                 .collectList();
     }
 
+    /**
+     * Method untuk mendapatkan top anime dengan limit 10
+     * @return -> Kumpulan 10 top anime (Based On Rank of MAL)
+     * @throws JikanQueryException
+     */
     public static Mono<List<Anime>> getTopAnime() throws JikanQueryException {
         return jikan.query().anime().top()
                 .limit(10)
@@ -228,13 +123,10 @@ public class JikanAPI {
                 .collectList();
     }
 
-//    public static Mono<List<Anime>> getRecommendationAnimeAsync() {
-//        return Flux.range(0, 5)
-//                .flatMap(i -> Mono.fromCallable(() -> jikan.query().random().anime().execute()))
-//                .flatMap(animeMono -> Mono.justOrEmpty(animeMono.block())) // Extract the Anime from Mono
-//                .collectList();
-//    }
-
+    /**
+     * Method untuk mendapatkan 40 rekomendasi anime (RANDOM ANIME)
+     * @return -> kumpulan rekomendasi anime atau random anime
+     */
     public static Mono<List<Anime>> getRecommendationAnimeAsync() {
         Jikan noCachingDek = new Jikan();
         return Flux.range(0, 40)
@@ -249,24 +141,11 @@ public class JikanAPI {
                 .collectList();
     }
 
-//    static int index = 0;
-//    public static Mono<List<Anime>> getAnimeInDatabaseUser(int[] id) {
-//        Jikan noCachingDek = new Jikan();
-//        index++;
-//        if (id.length == index) index = 0;
-//        return Flux.range(0, id.length)
-//                .flatMap(i -> Mono.defer(() -> {
-//                    try {
-//                        return Mono.justOrEmpty(noCachingDek.query().anime().get(id[index]).execute());
-//                    } catch (JikanQueryException e) {
-//                        return Mono.error(e);
-//                    }
-//                }))
-//                .flatMap(animeMono -> Mono.justOrEmpty(animeMono).block())
-//                .collectList();
-//    }
-
-
+    /**
+     * Method untuk mendapatkan anime dengan id tertentu melalui async java (CompletableFuture)
+     * @param id -> Id MAL anime
+     * @return -> CompletebleFUture dengan generic Objek Anime
+     */
     public static CompletableFuture<Anime> getAnimeByIdAsync(int id) {
         CompletableFuture<Anime> future = new CompletableFuture<>();
 
@@ -281,72 +160,4 @@ public class JikanAPI {
 
         return future;
     }
-
-//    public static List<CompletableFuture<Anime>> getAllAnimeListUserAsync() {
-//        List<CompletableFuture<Anime>> result = new ArrayList<>();
-//
-//        try (Connection connection = DatabaseConnection.getDataSource().getConnection()) {
-//            ConfigRepository configRepository = new ConfigRepositoryImpl();
-//            String sql = "select * from user_animelist where id_user = ?";
-//            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-//            preparedStatement.setInt(1, configRepository.getCurrentUserId());
-//
-//            ResultSet resultSet = preparedStatement.executeQuery();
-//
-//            while (resultSet.next()) {
-//                int animeId = resultSet.getInt("id_animeapi");
-//                CompletableFuture<Anime> animeFuture = CompletableFuture
-//                        .supplyAsync(() -> JikanAPI.getAnimeByIdAsync(animeId))
-//                        .thenCompose(Function.identity()); // Flatten the CompletableFuture
-//                result.add(animeFuture);
-//            }
-//
-//        } catch (SQLException e) {
-//            System.out.println(e.getMessage());
-//        }
-//
-//        return result;
-//    }
-
-    public static List<Anime> joinAllAsyncResults(List<CompletableFuture<Anime>> futures) {
-        CompletableFuture<Void> allOf = CompletableFuture.allOf(
-                futures.toArray(new CompletableFuture[0]));
-
-        try {
-            allOf.get(); // Wait for all to complete
-        } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        }
-
-        return futures.stream()
-                .map(CompletableFuture::join)
-                .collect(Collectors.toList());
-    }
-
-
-
-//    public static Mono<List<Anime>> getRecomendationAnime() throws JikanQueryException {
-//        Mono<List<Anime>> results = null;
-//        for (int i = 0; i < 5; i++) {
-//            Anime anime = jikan.query().random().anime().execute().block();
-//            results.
-//        }
-//
-//        return null;
-//    }
-//
-
-
-//    public static void main(String[] args) throws JikanQueryException {
-//        List<Anime> animeList = searchAnimeByTitle("naruto");
-//
-//        Object[][] result = animeToDataTable(animeList);
-//
-//        for (Object[] obj : result) {
-//            for (Object e : obj) {
-//                System.out.println(e);
-//            }
-//        }
-//    }
-
 }
