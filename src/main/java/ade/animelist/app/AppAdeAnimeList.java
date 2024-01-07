680379039ef179179fdda771ba4f6d0316ac3ac5
adenafil <nafilie9@gmail.com>
2024-01-07 15:50:15 +0700

    add java docs app

--- src/main/java/ade/animelist/app/AppAdeAnimeList.java
+++ src/main/java/ade/animelist/app/AppAdeAnimeList.java
@@ -1,9 +1,14 @@
 package ade.animelist.app;
-
 import ade.animelist.controller.Controller;
 
+/**
+ * Main class atau application
+ */
 public class AppAdeAnimeList {
-
+    /**
+     * Main Method dari program ini
+     * @param args
+     */
     public static void main(String[] args) {
         Controller.run();
     }
